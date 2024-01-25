f8da2123cb7835ac0ad132f969f131f92fbd833e
Kanchanamkl <kanchana.20212055@iit.ac.lk>
2024-01-25 23:02:27 +0530

    Cors origin allow enable changes
    Cors origin allow enable changes

--- src/main/java/com/scoutcomapss/api/security/auth/AuthenticationController.java
+++ src/main/java/com/scoutcomapss/api/security/auth/AuthenticationController.java
@@ -2,12 +2,10 @@ package com.scoutcomapss.api.security.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")// Specify the allowed origin(s)
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
--- src/main/java/com/scoutcomapss/api/security/config/ApplicationConfig.java
+++ src/main/java/com/scoutcomapss/api/security/config/ApplicationConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,6 +46,18 @@ public class ApplicationConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedOrigin("*"); // Allow all origins
+    config.addAllowedMethod("*"); // Allow all HTTP methods
+    config.addAllowedHeader("*"); // Allow all headers
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
   }
 
 }
