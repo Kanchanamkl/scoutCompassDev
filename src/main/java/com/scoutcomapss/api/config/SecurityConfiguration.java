833beb0ed5b8c4e71959c3ca9fa4d591fd01cd28
Kanchanamkl <kanchana.20212055@iit.ac.lk>
2024-03-08 11:11:53 +0530

    Quiz feature api level implementation.
    Quiz feature  api level implementation.

--- src/main/java/com/scoutcomapss/api/config/SecurityConfiguration.java
+++ src/main/java/com/scoutcomapss/api/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
                     .requestMatchers("/api/scoutcompass/auth/**").permitAll()
                     .requestMatchers("/api/scoutcompass/resource/**").permitAll()
                     .requestMatchers("/api/scoutcompass/profile/**").permitAll()
+                    .requestMatchers("/api/scoutcompass/requirement/**").permitAll()
                     //  .requestMatchers("/api/v1/test/**").permitAll()
                     // .requestMatchers("/api/v1/test/**").hasAuthority("ADMIN")
                     //  .requestMatchers(GET ,"/api/v1/demo-controller/**").permitAll()
